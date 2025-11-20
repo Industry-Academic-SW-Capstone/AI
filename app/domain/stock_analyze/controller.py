2f27035dd2da312f55d34421a36fb6fa65104466
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-11-21 02:41:33 +0900

    fix: dto수정

--- app/domain/stock_analyze/controller.py
+++ app/domain/stock_analyze/controller.py
@@ -4,6 +4,6 @@ from .service import analyze_stock
 
 router = APIRouter()
 
-@router.post("/analyze", response_model=StockAnalyzeResponse)
+@router.post("/analyze", response_model=StockAnalyzeResponse, response_model_by_alias=False)
 def stock_analyze_endpoint(request: StockAnalyzeRequest):
     return analyze_stock(request)
